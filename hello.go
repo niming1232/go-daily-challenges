fd08c131c11056633422da1246ff85a7836d5400
Shane <niming1232@gmail.com>
2021-11-07 16:15:14 +1100

    Test commit on vs code

--- hello.go
+++ hello.go
@@ -1,14 +1,18 @@
 package hello
 
 import (
-	"rsc.io/quote" // Different minor versions and patches can in the same path
+	"rsc.io/quote"            // Different minor versions and patches can in the same path
 	quoteV3 "rsc.io/quote/v3" // Different majoy versions in different paths (e.g. v1.2.3: 1 major, 2 minor, 3 patch)
 )
 
 func Hello() string {
-    return quote.Hello()
+	return quote.Hello()
 }
 
 func Proverb() string {
 	return quoteV3.Concurrency()
+}
+
+func Blah() string {
+	return "Blah blah blah."
 }
